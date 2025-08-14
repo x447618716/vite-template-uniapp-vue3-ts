28b16d27ec5efae5be77ef7ccb0375b1d652e8d2
xierongsheng <447618716@qq.com>
2025-08-14 15:35:43 +0800

    fix: 删除无用项

--- src/pages/mall/index.vue
+++
@@ -1,5 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-    <view class="container">页面构建中...</view>
-</template>
--- src/pages/organizationDetails/index.vue
+++
@@ -1,5 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-    <view>专题详情页</view>
-</template>
